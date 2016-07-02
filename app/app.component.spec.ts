e775aa2ec1b0eef980f4a8f0d6d0a3e8aab3c5e0
purplemass <bob@purplemass.com>
2016-07-02 12:58:39 +0100

    testing packages

---
+++ app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import { AppComponent } from './app.component'
+import {
+  async,
+  it,
+  describe,
+  expect,
+  inject,
+  beforeEach,
+  beforeEachProviders,
+} from '@angular/core/testing';
+
+describe('AppComponent', () => {
+
+  beforeEach(() => {
+    this.app = new AppComponent();
+  });
+
+  it('should have name property', () => {
+    expect(this.app.title).toBe('Angular 2 Bare');
+  });
+
+  // it('should say hello with name property', () => {
+  //   expect(this.app.sayHello()).toBe('Hello John');
+  // });
+
+});
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h2>Angular 2 Bare</h2>'
+  template: '<h2>{{title}}</h2>'
 })
 export class AppComponent {
-  constructor() { }
+  private title: string;
+
+  constructor() {
+    this.title = 'Angular 2 Bare';
+  }
 }
